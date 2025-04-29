27326d08ed482a092ca7b614572197d8bca59059
Anwar Hosain <anwarhosain1574@gmail.com>
2025-04-29 13:08:22 +0600

    modify pom, application file, remove unnecessary code

--- discovery-service/src/main/java/co/egen/somch/discovery/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/co/egen/somch/discovery/DiscoveryServiceApplication.java
@@ -1,18 +1,14 @@
 package co.egen.somch.discovery;
 
-import com.netflix.discovery.DiscoveryClient;
-import com.netflix.discovery.EurekaIdentityHeaderFilter;
-import com.sun.jersey.api.client.filter.ClientFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryServiceApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(DiscoveryServiceApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DiscoveryServiceApplication.class, args);
+  }
 
 }
